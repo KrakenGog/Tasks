9ddc3dd1b2da90bea760f26b3b1cee7b3df78ecc
100NOUT <100NOUT@DESKTOP-C1BTN94>
2023-09-09 20:29:26 +0300

    Добавьте файлы проекта.

---
+++ 3WaysToChangeVariable/3WaysToChangeVariable/3WaysToChangeVariable.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 2;
+
+    //1
+    //int c = a;
+    //a = b;
+    //b = c;
+    ////2
+    /*a = a + b;
+    b = a - b;
+    a = a - b;*/
+    //3
+    a = a - b;
+    b = a + b;
+    a = a + b;
+
+    cout << "a = " + to_string(a) << "b = " + to_string(b);
+
+    
+}
+
